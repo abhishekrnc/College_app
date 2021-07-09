be0c0e56d8c61c1344611819fd9d07b44765299a
Abhishek <abhishektupudana@gmail.com>
2021-07-10 00:26:57 +0530

    commit

--- app/src/main/java/me/iabhishek/college/MainActivity.java
+++ app/src/main/java/me/iabhishek/college/MainActivity.java
@@ -3,12 +3,31 @@ package me.iabhishek.college;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+//    For drawer
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView link = findViewById(R.id.ab);
+        link.setText(Html.fromHtml("<a href=https://www.google.com/>Click</a>"));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
+
+
+
+
+
+
     }
 }
